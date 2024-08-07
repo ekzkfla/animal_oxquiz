35885df164b8ed7ad526944f412d9bde8cfab44c
ekzkfla <godtoether@naver.com>
2024-08-08 00:50:16 +0900

    8/8 개발 진행 문제리스트 자바스크립트 정리 및 동작 스크립트 정리

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-var number_id, problem_id, answer_id;
-var problem_arr = [
-  "q1.png",
-  "q2.png",
-  "q3.png",
-  "q4.png",
-  "q5.png",
-  "qres.png",
-];
-var answer_arr = ["O", "X", "X", "O", "O"];
-
 var selectAnswer, score;
-var imgNum, startNum, endNum;
+var startNum, endNum;
+var number_id, problem_id, answer_id;
 
+//초기화
 function init() {
   number_id = document.getElementById("number_id");
   problem_id = document.getElementById("problem_id");
   answer_id = document.getElementById("answer_id");
 
   startNum = 0;
-  endNum = 5;
+  endNum = 6;
   score = 0;
-
   setProblem();
 }
 init();
 
+//문제 세팅
 function setProblem() {
-  problem_id.innerHTML =
-    "<img src='./img/" + problem_arr[startNum] + "' class='problem_img'>";
+  problem_id.innerHTML = problem_arr[1][startNum];
+  problem_id.setAttribute("answer", problem_arr[2][startNum]);
   if (startNum == endNum) {
+    problem_id.innerHTML = "종료!";
     number_id.innerHTML = "<span class='lable'><결과보기></span>";
     answer_id.innerHTML =
       "<button type='button' class='button res' onclick='btnResFunc();'> 결과보기</button><button type='button' class='button restart' onclick='history.go(0);'>다시하기</button>";
   } else {
-    number_id.innerHTML =
-      "<span class='label'><" + parseInt(startNum + 1) + "></spn>";
-    answer_id.innerHTML =
-      "<button type='button' class='button o' onclick='btnOFunc();'>O</button><button type='button' class='button x' onclick='btnXFunc();'>X</button>";
+    number_id.innerHTML = "<" + parseInt(problem_arr[0][startNum]) + ">";
   }
 }
 
 /* O버튼 함수 */
 function btnOFunc() {
+  answer = document.getElementById("problem_id").getAttribute("answer");
   selectAnswer = "O";
-  if (answer_arr[startNum] == selectAnswer) {
+  if (answer == selectAnswer) {
     score++;
   }
   startNum++;
+  // btnResFunc();    나중에 활성화
   setProblem();
 }
 
 /* X버튼 함수 */
 function btnXFunc() {
+  answer = document.getElementById("problem_id").getAttribute("answer");
   selectAnswer = "X";
-  if (answer_arr[startNum] == selectAnswer) {
+  if (answer == selectAnswer) {
     score++;
   }
   startNum++;
+  // btnResFunc(); 나중에 활성화
   setProblem();
 }
 
